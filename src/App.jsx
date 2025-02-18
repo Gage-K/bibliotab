bdc8a8e276d41ded20025c8e161d62b893adbf97
Gage-K <krausegage@gmail.com>
2025-02-17 23:12:39 -0500

    Update testing defaults

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,12 @@ import "./App.css";
 import TabDetails from "./components/TabDetails";
 
 // DATA IMPORTS
-import { tab } from "./data/defaultTab.json";
+import { defaultTab } from "./data/defaultTab.json";
 
 function App() {
-  const tabDetails = {
-    song: "Leave Them All Behind",
-    artist: "Ride",
-    creator: "Gage",
-    dateCreated: 20250214,
-    dateModified: 20250215,
-    tuning: "EADGBE",
-  };
-
-  console.log(tab);
-
-  const tab = [];
+  // constants for testing
+  const tabDetails = defaultTab.tabDetails; // general details about tab
+  const initTab = defaultTab.tab; // init testing notes for tab --> change later to empty init
 
   return (
     <>
