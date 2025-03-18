b292c044c0d12419a05923bed1bbd16ad5ed220a
Gage-K <krausegage@gmail.com>
2025-03-18 11:35:36 -0400

    Refactor styling and JSX structure

--- src/components/EditorControls.jsx
+++ src/components/EditorControls.jsx
@@ -1,5 +1,14 @@
 import PropTypes from "prop-types";
-import { CaretCircleDown, CaretCircleUp } from "@phosphor-icons/react";
+import {
+  FastForward,
+  Rewind,
+  Copy,
+  PencilSimple,
+  XCircle,
+  ColumnsPlusRight,
+  Plus,
+  Trash,
+} from "@phosphor-icons/react";
 
 export default function EditorControls({
   handleOpeningEditor,
@@ -8,30 +17,51 @@ export default function EditorControls({
   moveNext,
   duplicate,
   deleteFrame,
-  deleteMeasure,
   insertFrame,
   insertMeasure,
 }) {
+  const baseButton =
+    "border border-none p-2 font-medium rounded grid place-items-center duration-150 ease-in-out";
+
+  const buttonStyles = `${baseButton} hover:bg-neutral-700`;
+  const editButtonStyle = `${baseButton} bg-neutral-100 text-neutral-700 hover:text-neutral-700 hover:bg-neutral-300`;
+
+  const iconSize = 16;
   return (
-    <div className="tf-editor-controls">
-      <button onClick={handleOpeningEditor}>
-        {isOpen ? <CaretCircleUp /> : <CaretCircleDown />}
-        {isOpen ? " Close Editor" : " Open Editor"}
+    <div className="tf-editor-controls flex flex-wrap gap-4 my-2 place-content-center">
+      <button className={editButtonStyle} onClick={handleOpeningEditor}>
+        {isOpen ? (
+          <XCircle size={iconSize} />
+        ) : (
+          <PencilSimple size={iconSize} />
+        )}
       </button>
-      <div className="tf-editor-controls-supp">
-        <div></div>
-        <button onClick={movePrev}>Previous Position</button>
-        <button onClick={moveNext}>Next Position</button>
-        <button onClick={duplicate}>Duplicate</button>
-        <button onClick={insertFrame}>New Frame</button>
-        <button onClick={insertMeasure}>New Measure</button>
-        <button className="danger" onClick={deleteFrame}>
-          Delete Frame
-        </button>
-        <button className="danger" onClick={deleteMeasure}>
-          Delete Measure
-        </button>
-      </div>
+
+      <button className={buttonStyles} onClick={movePrev}>
+        <span className="hidden">Previous Position</span>
+        <Rewind size={iconSize} />
+      </button>
+      <button className={buttonStyles} onClick={moveNext}>
+        <span className="hidden">Next Position</span>
+        <FastForward size={iconSize} />
+      </button>
+      <button className={buttonStyles} onClick={insertFrame}>
+        <span className="hidden">New Frame</span>
+        <Plus size={iconSize} />
+      </button>
+      <button className={buttonStyles} onClick={insertMeasure}>
+        <div className="flex">
+          <span className="hidden">New Measure</span>
+          <ColumnsPlusRight size={iconSize} />
+        </div>
+      </button>
+      <button className={buttonStyles} onClick={duplicate}>
+        <span className="hidden">Duplicate Frame</span>
+        <Copy size={iconSize} />
+      </button>
+      <button className={buttonStyles} onClick={deleteFrame}>
+        <Trash size={iconSize} />
+      </button>
     </div>
   );
 }
