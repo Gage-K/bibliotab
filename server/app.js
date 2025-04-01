2be5bc64ffb2bd0123ee480f941ec413a1574dc4
Gage-K <krausegage@gmail.com>
2025-04-01 11:19:26 -0400

    Add dotenv and tests

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const tests = require("./src/tests/queryTests");
+require("dotenv").config();
+
+tests.runTests();
 
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = /* process.env.PORT || */ 3000;
 
 app.listen(PORT, () =>
   console.log(`TabLab Express app running on port ${PORT}`)
