4e3c0cb2d12ca2dc4b5aa9e06cb40277cdf15fdd
Gage-K <krausegage@gmail.com>
2025-04-01 11:19:47 -0400

    Fix port

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ tests.runTests();
 
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = /* process.env.PORT || */ 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
   console.log(`TabLab Express app running on port ${PORT}`)
