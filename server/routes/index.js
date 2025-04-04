6c154a4b35bc96618ed2af6d7bbb849646a01c3b
Gage-K <krausegage@gmail.com>
2025-04-04 11:30:02 -0400

    Add login and sign up routes

---
+++ server/routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res) => res.render("index"));
+router.get("/login", (req, res) => res.render("login"));
+router.get("/signup", (req, res) => res.render("signup"));
+
+module.exports = router;
