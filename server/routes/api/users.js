ad0fed3952d6cfb2b88539d5ba3f3e8f17a065e8
Gage-K <krausegage@gmail.com>
2025-04-13 17:36:12 -0400

    Remove authentication logic now located in controller

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,10 +1,27 @@
 const { Router } = require("express");
 const router = Router();
 const usersController = require("../../controllers/usersController");
+const passport = require("passport");
 
-router.get("/:id", usersController.getUser);
-router.put("/:id", usersController.updateUserEmail);
-router.post("/:id", usersController.insertUser);
-router.delete("/:id", usersController.deleteUser);
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  usersController.getUser
+); // need session false to ignore the session middleware
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  usersController.updateUserEmail
+);
+router.post(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  usersController.insertUser
+);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  usersController.deleteUser
+);
 
 module.exports = router;
