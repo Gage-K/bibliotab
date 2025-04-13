345953a0bcbbac9ca98edfa6092cb46e32403446
Gage-K <krausegage@gmail.com>
2025-04-13 17:38:44 -0400

    Refactor variables to be more amenable to API and improve readability

--- client/src/components/TabDetails.jsx
+++ client/src/components/TabDetails.jsx
@@ -7,55 +7,52 @@ import PropTypes from "prop-types";
 import {} from "@phosphor-icons/react";
 import { TablabContext } from "../layouts/TablabContextLayout";
 
-export default function TabDetails() {
-  const notes = [
-    "Ab",
-    "A",
-    "A#",
-    "Bb",
-    "B",
-    "C",
-    "C#",
-    "Db",
-    "D",
-    "D#",
-    "Eb",
-    "E",
-    "F",
-    "F#",
-    "Gb",
-    "G",
-    "G#",
-  ];
-  const strings = [1, 2, 3, 4, 5, 6];
+const notes = [
+  "Ab",
+  "A",
+  "A#",
+  "Bb",
+  "B",
+  "C",
+  "C#",
+  "Db",
+  "D",
+  "D#",
+  "Eb",
+  "E",
+  "F",
+  "F#",
+  "Gb",
+  "G",
+  "G#",
+];
+const strings = [1, 2, 3, 4, 5, 6];
 
+export default function TabDetails({ details, updateDetails }) {
   const [isShown, setIsShown] = useState(false);
-
-  const { id } = useContext(TabContext);
-  const { tabs, updateDetails } = useContext(TablabContext);
-
-  const currentTab = tabs.find((tab) => tab.id === id);
+  // const { id } = useContext(TabContext);
+  // const { tabs, updateDetails } = useContext(TablabContext);
+  // const currentTab = tabs.find((tab) => tab.id === id);
+  console.log("details:");
+  console.log(details);
 
   function handleTuning(event, string, value) {
-    const newTuning = currentTab.details.tuning.map((note, index) =>
+    const newTuning = details.tuning.map((note, index) =>
       index === string ? value : note
     );
-    updateDetails(id, event.target.name, newTuning);
+    updateDetails(event.target.name, newTuning);
   }
 
   return (
     <section className="tab-details tab-header mb-8">
       <div className="tab-details-container text-neutral-800 group">
         <div className="tab-details-top-wrapper flex gap-2 group">
-          <h1 className="text-5xl font-bold mb-4 mt-8">
-            {currentTab.details.song}
-          </h1>
+          <h1 className="text-5xl font-bold mb-4 mt-8">{details.song}</h1>
         </div>
 
         <div className="tab-details-sub-container flex flex-wrap gap-2 items-baseline text-neutral-500 font-medium">
           <p className="">
-            By {currentTab.details.artist}, tuning:{" "}
-            {currentTab.details.tuning.toReversed().join("")}
+            By {details.artist}, tuning: {details.tuning.toReversed().join("")}
           </p>
 
           <button
@@ -73,9 +70,9 @@ export default function TabDetails() {
           <input
             name="song"
             type="text"
-            value={currentTab.details.song}
+            value={details.song}
             onChange={(event) =>
-              updateDetails(id, event.target.name, event.target.value)
+              updateDetails(event.target.name, event.target.value)
             }
             className="px-2 py-1 mb-2 border border-neutral-400 rounded-sm text-neutral-600 focus-within:text-neutral-800"
           />
@@ -86,9 +83,9 @@ export default function TabDetails() {
           <input
             name="artist"
             type="text"
-            value={currentTab.details.artist}
+            value={details.artist}
             onChange={(event) =>
-              updateDetails(id, event.target.name, event.target.value)
+              updateDetails(event.target.name, event.target.value)
             }
             className="px-2 py-1 mb-2 border border-neutral-400 rounded-sm text-neutral-600 focus-within:text-neutral-800"></input>
           <label htmlFor="legend" className="font-semibold text-neutral-800">
@@ -112,7 +109,7 @@ export default function TabDetails() {
                   </label>
                   <select
                     name="tuning"
-                    value={currentTab.details.tuning[string - 1]}
+                    value={details.tuning[string - 1]}
                     onChange={(event) =>
                       handleTuning(event, string - 1, event.target.value)
                     }
