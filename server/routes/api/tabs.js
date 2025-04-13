d4d66238f0ffa562e5ea1984b55a8228f6397dae
Gage-K <krausegage@gmail.com>
2025-04-13 17:49:36 -0400

    Refactor routes with authentication

--- server/routes/api/tabs.js
+++ server/routes/api/tabs.js
@@ -1,11 +1,32 @@
 const { Router } = require("express");
 const router = Router();
 const tabsController = require("../../controllers/tabsController");
+const { isAuth } = require("../../middleware/authMiddleware");
 
-router.get("/", tabsController.getAllTabs);
-router.post("/", tabsController.createTab);
-router.get("/:tabId", tabsController.getTab);
-router.put("/:tabId", tabsController.updateTab);
-router.delete("/:tabId", tabsController.deleteTab);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  tabsController.getAllTabs
+);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  tabsController.createTab
+);
+router.get(
+  "/:tabId",
+  passport.authenticate("jwt", { session: false }),
+  tabsController.getTab
+);
+router.put(
+  "/:tabId",
+  passport.authenticate("jwt", { session: false }),
+  tabsController.updateTab
+);
+router.delete(
+  "/:tabId",
+  passport.authenticate("jwt", { session: false }),
+  tabsController.deleteTab
+);
 
 module.exports = router;
