277abdbde92bfbbd1b61a4bf351bda0d9f600d05
Gage-K <krausegage@gmail.com>
2025-04-25 17:37:56 -0400

    Remove unused import

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,6 +1,5 @@
 const passport = require("passport");
 const db = require("../db/queries");
-const bcrypt = require("bcryptjs");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 require("dotenv").config();
