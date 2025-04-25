23ea9e0edf9e4eb9c6ff117d3807b8ce223e96d9
Gage-K <krausegage@gmail.com>
2025-04-25 17:40:36 -0400

    Clarify comments on Auth hook

--- client/src/hooks/useAuth.jsx
+++ client/src/hooks/useAuth.jsx
@@ -2,8 +2,8 @@ import { useContext } from "react";
 import AuthContext from "../context/authProvider";
 
 export default function useAuth() {
-  // intent is to take info from AuthProvider
-  // helps us avoid having to import the context on any component that requires authorization
-  // custom hook makes this easier and more streamlined
+  // Receive info from AuthProvider.
+  // This avoids needing to import the context on any component that requires authorization.
+  // Custom hook streamlines this process.
   return useContext(AuthContext);
 }
