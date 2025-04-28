a00d08a6c8e17983b4e0b628a3eb2856f89932d3
Gage-K <krausegage@gmail.com>
2025-04-28 08:22:54 -0400

    Update pool connection

--- server/db/pool.js
+++ server/db/pool.js
@@ -2,17 +2,18 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const connectionURL = process.env.DB_URL;
-console.log("connection url:");
-console.log(connectionURL);
-console.log(process.env.NODE_ENV);
 
-module.exports =
-  process.env.NODE_ENV === "prod"
-    ? new Pool({ connectionURL, ssl: { rejectUnauthorized: false } })
-    : new Pool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-      });
+module.exports = new Pool({
+  connectionURL,
+  ssl: { rejectUnauthorized: false },
+});
+
+/*
+new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
+*/
